7865567415bd8b319a4afed76b7e8ff310164be2
Chris Leung <leung_chris@bah.com>
2020-07-27 12:33:51 -0700

    commit gradle project

---
+++ event-registration-gradle/src/main/java/com/bah/eventregistrationgradle/eventregistrationgradle/EventRegistrationGradleApplication.java
@@ -0,0 +1,13 @@
+package com.bah.eventregistrationgradle.eventregistrationgradle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventRegistrationGradleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventRegistrationGradleApplication.class, args);
+	}
+
+}
---
+++ event-registration-gradle/src/test/java/com/bah/eventregistrationgradle/eventregistrationgradle/EventRegistrationGradleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bah.eventregistrationgradle.eventregistrationgradle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventRegistrationGradleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
